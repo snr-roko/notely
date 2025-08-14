93e80c577244c54234bf80f635c2baddefabc471
snr-roko <rbbagyei@outlook.com>
2025-08-14 07:35:35 +0000

    feat: connect to MongoDB and update Note model serialization

--- notely-backend/app.js
+++ notely-backend/app.js
@@ -1,7 +1,17 @@
 const express = require('express')
-const {port} = require('./utils/config')
+const {port, mongoUrl} = require('./utils/config')
 const app = express()
+const mongoose = require('mongoose')
+const notesRouter = require('./routes/noteRouter')
 
+mongoose.connect(mongoUrl).then(() => {
+  console.log('Connected to dateabase successfully')
+}).catch(error => {
+  console.log('Error connecting to database: ', error.message)
+})
+
+app.use(express.json())
+app.use('/api/notes', notesRouter)
 
 app.listen(port, () => {
   console.log(`Notely Backend started on port ${port}`)
--- notely-backend/models/note.js
+++ notely-backend/models/note.js
@@ -30,4 +30,12 @@ noteSchema.pre('findOneAndUpdate', (next) => {
   next()
 })
 
-module.export = mongoose.model('Note', noteSchema)
+mongoose.set('toJSON', {
+  transform: (returnedObject) =>{
+    returnedObject.id = returnedObject._id.toString(),
+    delete returnedObject._id,
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
