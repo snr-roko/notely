56135f52037b69677d14b8ee059cc6150cdcf0a3
snr-roko <rbbagyei@outlook.com>
2025-08-15 06:57:53 +0000

    fix: correct toJSON transformation method in note schema

--- notely-backend/models/note.js
+++ notely-backend/models/note.js
@@ -30,8 +30,8 @@ noteSchema.pre('findOneAndUpdate', (next) => {
   next()
 })
 
-mongoose.set('toJSON', {
-  transform: (returnedObject) =>{
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) =>{
     returnedObject.id = returnedObject._id.toString(),
     delete returnedObject._id,
     delete returnedObject.__v
